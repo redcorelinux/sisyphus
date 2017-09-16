579a8d3b4efff9ac443fc019ddf441a8d4dd184c
V3n3RiX <venerix@redcorelinux.org>
2017-09-16 14:09:12 +0100

    rearrange backend && cli frontend code

--- src/frontend/cli/sisyphus-cli.py
+++ src/frontend/cli/sisyphus-cli.py
@@ -10,22 +10,22 @@ PKGLIST = sys.argv[2:]
 if "__main__" == __name__:
     if "install" in sys.argv[1:]:
         sisyphus_pkg_install(PKGLIST)
-    elif "uninstall" in sys.argv[1:]:
-        sisyphus_pkg_uninstall(PKGLIST)
-    elif "force-uninstall" in sys.argv[1:]:
-        sisyphus_pkg_force_uninstall(PKGLIST)
-    elif "remove-orphans" in sys.argv[1:]:
-        sisyphus_pkg_remove_orphans()
-    elif "upgrade" in sys.argv[1:]:
-        sisyphus_pkg_system_upgrade()
     elif "auto-install" in sys.argv[1:]:
         sisyphus_pkg_auto_install(PKGLIST)
+    elif "uninstall" in sys.argv[1:]:
+        sisyphus_pkg_uninstall(PKGLIST)
     elif "auto-uninstall" in sys.argv[1:]:
         sisyphus_pkg_auto_uninstall(PKGLIST)
+    elif "force-uninstall" in sys.argv[1:]:
+        sisyphus_pkg_force_uninstall(PKGLIST)
     elif "auto-force-uninstall" in sys.argv[1:]:
         sisyphus_pkg_auto_force_uninstall(PKGLIST)
+    elif "remove-orphans" in sys.argv[1:]:
+        sisyphus_pkg_remove_orphans()
     elif "auto-remove-orphans" in sys.argv[1:]:
         sisyphus_pkg_auto_remove_orphans()
+    elif "upgrade" in sys.argv[1:]:
+        sisyphus_pkg_system_upgrade()
     elif "auto-upgrade" in sys.argv[1:]:
         sisyphus_pkg_auto_system_upgrade()
     elif "search" in sys.argv[1:]:
