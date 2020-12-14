9163df2c61b1b79cb14a4358e304b31756384077
V3n3RiX <venerix@redcorelinux.org>
2020-12-14 13:14:08 +0000

    set the correct profile

--- src/backend/setprofile.py
+++ src/backend/setprofile.py
@@ -5,7 +5,7 @@ import subprocess
 
 @animation.wait('setting up hardened profile')
 def start():
-    eselectExec = subprocess.Popen(['eselect', 'profile', 'set', 'default/linux/amd64/17.0/hardened'])
+    eselectExec = subprocess.Popen(['eselect', 'profile', 'set', 'default/linux/amd64/17.1/hardened'])
     eselectExec.wait()
     envExec = subprocess.Popen(['env-update'], stdout=subprocess.DEVNULL)
     envExec.wait()
