e4f2e722b2740d16ddc2ffa9b184287a7850b203
V3n3RiX <venerix@redcorelinux.org>
2021-01-31 01:19:56 +0000

    backend : reuse binhost module in csvfiles module, one less portage call

--- src/backend/update.py
+++ src/backend/update.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 import animation
+import sys
 import sisyphus.cache
 import sisyphus.check
 import sisyphus.database
@@ -11,28 +12,33 @@ import sisyphus.sync
 def start():
     sisyphus.cache.clean()
 
-    needsPortage = sisyphus.check.portage()
-    needsOverlay = sisyphus.check.overlay()
+    mismatch = sisyphus.check.branch()
 
-    if needsPortage == 1:
-        if needsOverlay == 1:
-            sisyphus.sync.portage()
-            sisyphus.sync.overlay()
-            sisyphus.sync.portageCfg()
-            sisyphus.database.syncRemote()
-            sisyphus.metadata.regenSilent()
-        elif not needsOverlay == 1:
-            sisyphus.sync.portage()
-            sisyphus.sync.overlay()
-            sisyphus.sync.portageCfg()
-            sisyphus.database.syncRemote()
-            sisyphus.metadata.regenSilent()
-    elif not needsPortage == 1:
-        if needsOverlay == 1:
-            sisyphus.sync.portage()
-            sisyphus.sync.overlay()
-            sisyphus.sync.portageCfg()
-            sisyphus.database.syncRemote()
-            sisyphus.metadata.regenSilent()
-        elif not needsOverlay == 1:
-            sisyphus.sync.portageCfg()
+    if mismatch == 0:
+        needsPortage = sisyphus.check.portage()
+        needsOverlay = sisyphus.check.overlay()
+
+        if needsPortage == 1:
+            if needsOverlay == 1:
+                sisyphus.sync.portage()
+                sisyphus.sync.overlay()
+                sisyphus.sync.portageCfg()
+                sisyphus.database.syncRemote()
+                sisyphus.metadata.regenSilent()
+            elif not needsOverlay == 1:
+                sisyphus.sync.portage()
+                sisyphus.sync.overlay()
+                sisyphus.sync.portageCfg()
+                sisyphus.database.syncRemote()
+                sisyphus.metadata.regenSilent()
+        elif not needsPortage == 1:
+            if needsOverlay == 1:
+                sisyphus.sync.portage()
+                sisyphus.sync.overlay()
+                sisyphus.sync.portageCfg()
+                sisyphus.database.syncRemote()
+                sisyphus.metadata.regenSilent()
+            elif not needsOverlay == 1:
+                sisyphus.sync.portageCfg()
+    else:
+        sys.exit("\n" + "")
