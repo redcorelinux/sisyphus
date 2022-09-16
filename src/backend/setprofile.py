0cf2c9f8ec52b1ae276c1573210d252ab0c4577b
V3n3RiX <venerix@koprulu.sector>
2022-09-16 09:50:21 +0100

    replace subprocess.Popen.wait with subprocess.Popen.communicate everywhere we use pipes to avoid deadlocks

--- src/backend/setprofile.py
+++ src/backend/setprofile.py
@@ -7,12 +7,12 @@ import subprocess
 @animation.wait('setting up profile')
 def start():
     if platform.uname()[4] == 'x86_64':
-        eselectExec = subprocess.Popen(['eselect', 'profile', 'set', 'default/linux/amd64/17.1/hardened'])
-        eselectExec.wait()
+        eselectExec = subprocess.Popen(['eselect', 'profile', 'set', 'default/linux/amd64/17.1/hardened'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        stdout, stderr = eselectExec.communicate()
 
     if platform.uname()[4] == 'aarch64':
-        eselectExec = subprocess.Popen(['eselect', 'profile', 'set', 'default/linux/arm64/17.0'])
-        eselectExec.wait()
+        eselectExec = subprocess.Popen(['eselect', 'profile', 'set', 'default/linux/arm64/17.0'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        stdout, stderr = eselectExec.communicate()
 
     envExec = subprocess.Popen(['env-update'], stdout=subprocess.DEVNULL)
     envExec.wait()
