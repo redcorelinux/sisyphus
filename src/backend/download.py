30f0ed9e0fa8b93b98ce707b4b432c3959dc764c
V3n3RiX <venerix@koprulu.sector>
2022-11-04 01:16:23 +0000

    backend : maintain some consistency, shave off some more lines of code

--- src/backend/download.py
+++ src/backend/download.py
@@ -7,10 +7,8 @@ import wget
 import sisyphus.getenv
 import sisyphus.getfs
 
-def package(pkgname):
-    binhostURL = sisyphus.getenv.binhostURL()
-    areBinaries,areSources,needsConfig = pickle.load(open(os.path.join(sisyphus.getfs.portageMetadataDir, "sisyphus_solvedeps_pkg.pickle"), "rb"))
 
+def fetchAll():
     for index, binary in enumerate([package + '.tbz2' for package in areBinaries], start=1):
         print(">>> Downloading binary ({}".format(index) + " " + "of" + " " + str(len(areBinaries)) + ")" + " " + binary)
         wget.download(binhostURL + binary)
@@ -24,22 +22,15 @@ def package(pkgname):
 
         if os.path.exists(binary.rstrip().split("/")[1]):
             os.remove(binary.rstrip().split("/")[1])
+
+
+def pkg(pkgname):
+    binhostURL = sisyphus.getenv.binhostURL()
+    areBinaries,areSources,needsConfig = pickle.load(open(os.path.join(sisyphus.getfs.portageMetadataDir, "sisyphus_solvedeps_pkg.pickle"), "rb"))
+    fetchAll()
 
 
 def world():
     binhostURL = sisyphus.getenv.binhostURL()
     areBinaries,areSources,needsConfig = pickle.load(open(os.path.join(sisyphus.getfs.portageMetadataDir, "sisyphus_solvedeps_world.pickle"), "rb"))
-
-    for index, binary in enumerate([package + '.tbz2' for package in areBinaries], start=1):
-        print(">>> Downloading binary ({}".format(index) + " " + "of" + " " + str(len(areBinaries)) + ")" + " " + binary)
-        wget.download(binhostURL + binary)
-        print("")
-
-        if os.path.isdir(os.path.join(sisyphus.getfs.portageCacheDir, binary.rstrip().split("/")[0])):
-            shutil.move(binary.rstrip().split("/")[1], os.path.join(os.path.join(sisyphus.getfs.portageCacheDir, binary.rstrip().split("/")[0]), os.path.basename(binary.rstrip().split("/")[1])))
-        else:
-            os.makedirs(os.path.join(sisyphus.getfs.portageCacheDir, binary.rstrip().split("/")[0]))
-            shutil.move(binary.rstrip().split("/")[1], os.path.join(os.path.join(sisyphus.getfs.portageCacheDir, binary.rstrip().split("/")[0]), os.path.basename(binary.rstrip().split("/")[1])))
-
-        if os.path.exists(binary.rstrip().split("/")[1]):
-            os.remove(binary.rstrip().split("/")[1])
+    fetchAll()
