0290e7d7293d0f5df6ccba6afe5b0b35b43c2e5a
V3n3RiX <venerix@koprulu.sector>
2023-03-10 05:36:08 +0000

    eselectExec -> e_exe; envExec -> env_exe

--- src/backend/setprofile.py
+++ src/backend/setprofile.py
@@ -8,14 +8,14 @@ import subprocess
 @animation.wait('setting up profile')
 def start():
     if platform.uname()[4] == 'x86_64':
-        eselectExec = subprocess.Popen(
+        e_exe = subprocess.Popen(
             ['eselect', 'profile', 'set', 'default/linux/amd64/17.1/hardened'])
-        eselectExec.wait()
+        e_exe.wait()
 
     if platform.uname()[4] == 'aarch64':
-        eselectExec = subprocess.Popen(
+        e_exe = subprocess.Popen(
             ['eselect', 'profile', 'set', 'default/linux/arm64/17.0'])
-        eselectExec.wait()
+        e_exe.wait()
 
-    envExec = subprocess.Popen(['env-update'], stdout=subprocess.DEVNULL)
-    envExec.wait()
+    env_exe = subprocess.Popen(['env-update'], stdout=subprocess.DEVNULL)
+    env_exe.wait()
