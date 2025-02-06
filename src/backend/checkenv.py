5ad866a13b4754cbdf56f8f6c85f1debcd8d06c6
V3n3RiX <venerix@koprulu.sector>
2025-02-06 12:00:42 +0000

    rename network check config file

--- src/backend/checkenv.py
+++ src/backend/checkenv.py
@@ -11,7 +11,7 @@ from urllib.error import HTTPError
 
 
 NETWORK_CHECK_URL_FILE = os.path.join(
-    sisyphus.getfs.s_cfg_dir, "network_check_url.conf")
+    sisyphus.getfs.s_cfg_dir, "sisyphus.net_chk_addr.conf")
 
 
 def is_valid_url(url):
