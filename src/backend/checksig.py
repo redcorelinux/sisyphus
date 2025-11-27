b12300e07d23c40fd297ea4e44fa840d6e813bef
V3n3RiX <venerix@koprulu.sector>
2025-11-27 16:51:12 +0000

    checksig : halt on KeyboardInterrupt

--- src/backend/checksig.py
+++ src/backend/checksig.py
@@ -16,19 +16,28 @@ KEY_PATH = os.path.join(
 
 def start():
     tree = sisyphus.getfs.g_src_dir
-    g_exec = subprocess.Popen(['gemato', 'verify', tree, '-s', '-K', KEY_PATH],
-                              stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, encoding='utf-8')
+    try:
+        g_exec = subprocess.Popen(['gemato', 'verify', tree, '-s', '-K', KEY_PATH],
+                                  stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, encoding='utf-8')
 
-    for line in g_exec.stdout:
-        print(line, end='')
+        for line in g_exec.stdout:
+            print(line, end='')
 
-    g_exec.wait()
+        g_exec.wait()
 
-    err = g_exec.stderr.read()
-    if err:
-        print(err, file=sys.stderr)
+        err = g_exec.stderr.read()
+        if err:
+            print(err, file=sys.stderr)
 
-    if g_exec.returncode != 0:
-        print(
-            f"\nPortage tree signature verification failed for '{tree}'\n", file=sys.stderr,)
-        sys.exit(g_exec.returncode)
+        if g_exec.returncode != 0:
+            print(
+                f"\nPortage tree signature verification failed for '{tree}'\n", file=sys.stderr,)
+            sys.exit(g_exec.returncode)
+    except KeyboardInterrupt:
+        print("\nPortage tree signature verification interrupted.", file=sys.stderr)
+        g_exec.terminate()
+        try:
+            g_exec.wait(1)
+        except subprocess.TimeoutExpired:
+            g_exec.kill()
+        sys.exit()
