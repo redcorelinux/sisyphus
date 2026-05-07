308dda312dfdefb446a23b8880efaaec48a872db
V3n3RiX <venerix@koprulu.sector>
2026-05-07 07:16:50 +0100

    BINREPOS doesn't expose itself in 'emerge --info --verbose' like BINHOST does, so get the csv url's straight from the config

--- src/backend/getenv.py
+++ src/backend/getenv.py
@@ -1,71 +1,67 @@
 #!/usr/bin/python3
 
-import io
 import os
-import signal
 import subprocess
 import sisyphus.getfs
 
 
-def sigint_handler(signal, frame):
-    sys.exit(0)
-
-
-signal.signal(signal.SIGINT, sigint_handler)
-
-
 def binhost_addr():
-    binhost_addr = []
-    p_exe = subprocess.Popen(
-        ['emerge', '--info', '--verbose'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    files = [
+        (sisyphus.getfs.binhostcfg, 'BINHOST (DEPRECATED)'),
+        (sisyphus.getfs.binreposcfg, 'BINREPOS (CURRENT)')
+    ]
 
-    for p_out in io.TextIOWrapper(p_exe.stdout, encoding="utf-8"):
-        if "PORTAGE_BINHOST" in p_out:
-            binhost_addr = p_out.rstrip().split("=")[1].strip('\"')
-    try:
-        p_exe.wait()
-    except KeyboardInterrupt:
-        p_exe.terminate()
-        try:
-            p_exe.wait(1)
-        except subprocess.TimeoutExpired:
-            p_exe.kill()
-        sys.exit()
+    for path, ftype in files:
+        if not os.path.exists(path):
+            continue
 
-    return binhost_addr
+        with open(path, 'r') as f:
+            for line in f:
+                clean = line.strip()
+                if clean.startswith('#') or not clean:
+                    continue
+
+                if ftype == 'BINHOST (DEPRECATED)' and 'PORTAGE_BINHOST=' in clean:
+                    return clean.split("=", 1)[1].replace('"', '').replace("'", "").strip()
+
+                if ftype == 'BINREPOS (CURRENT)' and 'sync-uri =' in clean:
+                    return clean.split("=", 1)[1].strip()
+    return ""
 
 
 def csv_addr():
-    csv_addr = binhost_addr()
-    pkg_csv_addr = []
-    desc_csv_addr = []
+    active_url = binhost_addr()
+    if not active_url:
+        return None, None
 
-    if "packages-next" in csv_addr:
-        pkg_csv_addr = csv_addr.replace(
+    if "packages-next" in active_url:
+        pkg_csv = active_url.replace(
             'packages-next', 'csv-next') + 'remotePackagesPre.csv'
-        desc_csv_addr = csv_addr.replace(
+        desc_csv = active_url.replace(
             'packages-next', 'csv-next') + 'remoteDescriptionsPre.csv'
     else:
-        pkg_csv_addr = csv_addr.replace(
+        pkg_csv = active_url.replace(
             'packages', 'csv') + 'remotePackagesPre.csv'
-        desc_csv_addr = csv_addr.replace(
+        desc_csv = active_url.replace(
             'packages', 'csv') + 'remoteDescriptionsPre.csv'
 
-    return pkg_csv_addr, desc_csv_addr
+    return pkg_csv, desc_csv
 
 
 def system_branch():
     active_branch = None
+    ebuild_dir = sisyphus.getfs.gentoo_ebuild_dir
 
-    if os.path.isdir(os.path.join(sisyphus.getfs.gentoo_ebuild_dir, '.git')):
-        os.chdir(sisyphus.getfs.gentoo_ebuild_dir)
-        local_branch = subprocess.check_output(
-            ['git', 'rev-parse', '--abbrev-ref', 'HEAD'])
+    if os.path.isdir(os.path.join(ebuild_dir, '.git')):
+        try:
+            local_branch = subprocess.check_output(
+                ['git', '-C', ebuild_dir, 'rev-parse', '--abbrev-ref', 'HEAD'],
+                stderr=subprocess.DEVNULL
+            ).decode().strip()
 
-        if local_branch.decode().strip() == 'master':
-            active_branch = str('master')
-
-        if local_branch.decode().strip() == 'next':
-            active_branch = str('next')
+            if local_branch in ['master', 'next']:
+                active_branch = local_branch
+        except subprocess.CalledProcessError:
+            pass
 
     return active_branch
